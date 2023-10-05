ea2baf9ceb5a93152e3831fa7752074d71c4d728
Jodrix-15 <146071125+Jodrix-15@users.noreply.github.com>
2023-10-05 12:59:49 +0200

    Add files via upload

---
+++ tasca03/tasca03/src/nivel1/Month.java
@@ -0,0 +1,19 @@
+package nivel1;
+
+public class Month {
+	
+	/*Crea una classe anomenada Month amb un atribut "name" (que emmagatzemarà 
+		 * el nom del mes de l'any). .*/
+	
+	private String name;
+	
+	public Month(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+}
