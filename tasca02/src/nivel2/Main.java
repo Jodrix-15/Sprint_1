0f384d4b5c5b2daf9d2c9bb0e871d45e49b1fed2
Jodrix-15 <146071125+Jodrix-15@users.noreply.github.com>
2023-10-07 11:09:40 +0200

    Add files via upload

--- tasca02/src/nivel2/Main.java
+++ tasca02/src/nivel2/Main.java
@@ -31,7 +31,7 @@ public class Main {
 		boolean esChr;
 		
 		
-		/*do {
+		do {
 			try {
 				
 				numByte = ex.llegirByte("Introduce un numero entre -128 y 127: ");
@@ -40,9 +40,9 @@ public class Main {
 				System.out.println("Debe ser un numero entre -128 y 127\n");
 				esByte = false;
 			}
-		}while(esByte == false);*/
+		}while(esByte == false);
 		
-		/*do {
+		do {
 			try {
 				
 				numInt = ex.llegirInt("Introduce un numero entre -2.147.483.648 y 2.147.483.649: ");
@@ -51,9 +51,9 @@ public class Main {
 				System.out.println("Debe ser un numero entre -2.147.483.648 y 2.147.483.649\n");
 				esInt = false;
 			}
-		}while(esInt == false);*/
+		}while(esInt == false);
 		
-		/*do {
+		do {
 			try {
 				
 				numFloat = ex.llegirFloat("Introduce un numero entre -3.402823e38 y 3.402823e38: ");
@@ -62,9 +62,9 @@ public class Main {
 				System.out.println("Debe ser un numero entre -3.402823e38 y 3.402823e38\n");
 				esFloat = false;
 			}
-		}while(esFloat == false);*/
+		}while(esFloat == false);
 		
-		/*do {
+		do {
 			try {
 				
 				numDouble = ex.llegirFloat("Introduce un numero entre -	-1.79769313486232e308 y 3.402823e38: ");
@@ -73,9 +73,9 @@ public class Main {
 				System.out.println("Debe ser un numero entre -	-1.79769313486232e308 y 1.79769313486232e308\n");
 				esDouble = false;
 			}
-		}while(esDouble == false);*/
+		}while(esDouble == false);
 		
-		/*do {
+		do {
 			try {
 				
 				chr = ex.llegirChar("Introduce una letra: ");
@@ -84,10 +84,10 @@ public class Main {
 				System.out.println("Debes escribir una caracter\n");
 				esChar = false;
 			}
-		}while(esChar == false);*/
+		}while(esChar == false);
 		
 	
-		//str = ex.llegirString("Introduce un texto: ");
+		str = ex.llegirString("Introduce un texto: ");
 		
 		do {
 			try {
