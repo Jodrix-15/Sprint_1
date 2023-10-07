55e5d0c4c4034e4edd057e32c07170879b0855d5
Jodrix-15 <146071125+Jodrix-15@users.noreply.github.com>
2023-10-07 12:01:50 +0200

    Add files via upload

--- tasca02/src/nivel2/Main.java
+++ tasca02/src/nivel2/Main.java
@@ -1,5 +1,6 @@
 package nivel2;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,98 +14,142 @@ public class Main {
 		 * de seguir demanant la dada.
 		 */
 		
+		Scanner sc = new Scanner(System.in);
 		Entrada ex = new Entrada();
-		boolean esByte = false;
-		boolean esInt = false;
-		boolean esFloat = false;
-		boolean esDouble = false;
-		boolean esChar = false;
-		boolean esString = false;
-		boolean esBoolean = false;
 		
-		byte numByte = 0;
-		int numInt = 0;
-		float numFloat = 0;
-		double numDouble = 0;
-		char chr;
-		String str;
-		boolean esChr;
+		int opcion = -1;
 		
 		
 		do {
+			menu();
 			try {
+				opcion = ex.llegirInt("\nElige una opcion: ");
 				
-				numByte = ex.llegirByte("Introduce un numero entre -128 y 127: ");
-				esByte=true;
-			}catch (InputMismatchException e) {
-				System.out.println("Debe ser un numero entre -128 y 127\n");
-				esByte = false;
+			}catch(InputMismatchException e) {
+				System.out.println("Debe ser un numero entero");
 			}
-		}while(esByte == false);
-		
-		do {
-			try {
+			
+			switch (opcion) {
+			case 1:
+				boolean esByte = false;
+				byte numByte = 0;
+				do {
+					try {
+						
+						numByte = ex.llegirByte("Introduce un numero entre -128 y 127: ");
+						System.out.println("Valor introducido correctamente");
+						esByte=true;
+					}catch (InputMismatchException e) {
+						System.out.println("Debe ser un numero entre -128 y 127\n");
+						esByte = false;
+					}
+				}while(esByte == false);
+				break;
+			case 2:
+				boolean esInt = false;
+				int numInt = 0;
 				
-				numInt = ex.llegirInt("Introduce un numero entre -2.147.483.648 y 2.147.483.649: ");
-				esInt=true;
-			}catch (InputMismatchException e) {
-				System.out.println("Debe ser un numero entre -2.147.483.648 y 2.147.483.649\n");
-				esInt = false;
+				do {
+					try {
+						
+						numInt = ex.llegirInt("Introduce un numero entre -2.147.483.648 y 2.147.483.649: ");
+						System.out.println("Valor introducido correctamente");
+						esInt=true;
+					}catch (InputMismatchException e) {
+						System.out.println("Debe ser un numero entre -2.147.483.648 y 2.147.483.649\n");
+						esInt = false;
+					}
+				}while(esInt == false);
+				break;
+			case 3:
+				boolean esFloat = false;
+				float numFloat = 0;
+				
+				do {
+					try {
+						
+						numFloat = ex.llegirFloat("Introduce un numero entre -3.402823e38 y 3.402823e38: ");
+						System.out.println("Valor introducido correctamente");
+						esFloat=true;
+					}catch (InputMismatchException e) {
+						System.out.println("Debe ser un numero entre -3.402823e38 y 3.402823e38\n");
+						esFloat = false;
+					}
+				}while(esFloat == false);
+				break;
+			case 4:
+				boolean esDouble = false;
+				double numDouble = 0;
+				
+				do {
+					try {
+						
+						numDouble = ex.llegirDouble("Introduce un numero entre -1.79769313486232e308 y 3.402823e38: ");
+						System.out.println("Valor introducido correctamente");
+						esDouble=true;
+					}catch (InputMismatchException e) {
+						System.out.println("Debe ser un numero entre -1.79769313486232e308 y 1.79769313486232e308\n");
+						esDouble = false;
+					}
+				}while(esDouble == false);
+				break;
+			case 5:
+				boolean esChar = false;
+				char chr;
+				
+				do {
+					try {
+						
+						chr = ex.llegirChar("Introduce una letra: ");
+						System.out.println("Valor introducido correctamente");
+						esChar=true;
+					}catch (Exception e) {
+						System.out.println("Debes escribir una caracter\n");
+						esChar = false;
+					}
+				}while(esChar == false);
+				break;
+			case 6:
+				String str;
+				
+				str = ex.llegirString("Introduce un texto: ");
+				System.out.println("Valor introducido correctamente");
+				break;
+			case 7:
+				boolean esBoolean = false;
+				boolean esChr;
+				
+				do {
+					try {
+						
+						esChr = ex.llegirSiNo("Introduce una letra [s]i o [n]o: ");
+						System.out.println("Valor introducido correctamente");
+						esBoolean = true;
+					}catch (Exception e) {
+						System.out.println("Debes escribir 's' o 'n'\n");	
+						esBoolean = false;
+						
+					}
+				}while(esBoolean == false);
+				break;
 			}
-		}while(esInt == false);
-		
-		do {
-			try {
-				
-				numFloat = ex.llegirFloat("Introduce un numero entre -3.402823e38 y 3.402823e38: ");
-				esFloat=true;
-			}catch (InputMismatchException e) {
-				System.out.println("Debe ser un numero entre -3.402823e38 y 3.402823e38\n");
-				esFloat = false;
-			}
-		}while(esFloat == false);
-		
-		do {
-			try {
-				
-				numDouble = ex.llegirFloat("Introduce un numero entre -	-1.79769313486232e308 y 3.402823e38: ");
-				esDouble=true;
-			}catch (InputMismatchException e) {
-				System.out.println("Debe ser un numero entre -	-1.79769313486232e308 y 1.79769313486232e308\n");
-				esDouble = false;
-			}
-		}while(esDouble == false);
-		
-		do {
-			try {
-				
-				chr = ex.llegirChar("Introduce una letra: ");
-				esChar=true;
-			}catch (Exception e) {
-				System.out.println("Debes escribir una caracter\n");
-				esChar = false;
-			}
-		}while(esChar == false);
-		
-	
-		str = ex.llegirString("Introduce un texto: ");
-		
-		do {
-			try {
-				
-				esChr = ex.llegirSiNo("Introduce una letra [s]i o [n]o: ");
-				esBoolean = true;
-			}catch (Exception e) {
-				System.out.println("Debes escribir 's' o 'n'\n");	
-				esBoolean = false;
-				
-			}
-		}while(esBoolean == false);
-		
+		}while(opcion != 0);
 		
 
 	}
 	
-	
+	public static void menu() {
+		
+		System.out.println("De las siguientes opciones:\n"
+				+ "\n1. Leer Byte\n"
+				+ "2. Leer Int\n"
+				+ "3. Leer Float\n"
+				+ "4. Leer Double\n"
+				+ "5. Leer Char\n"
+				+ "6. Leer String\n"
+				+ "7. Leer Si No\n"
+				+ "0. Salir");
+		
+	}
 
 }
